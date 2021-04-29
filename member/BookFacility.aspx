829c2e9c8605b6df09c5ae79583c4af0ca3856e5
CamillaMoller <63704900+CamillaMoller@users.noreply.github.com>
2021-04-29 15:37:57 +0200

    Happy

--- member/BookFacility.aspx
+++ member/BookFacility.aspx
@@ -14,11 +14,46 @@
    <br /><br />
 
    <asp:Label ID="facilityLabel" runat="server" Width="60px" Text="Facility:"></asp:Label>
-   <asp:Label ID="facilityValueLabel" runat="server" Width="60px" Text="Facility"></asp:Label>
+    <asp:DropDownList ID="FacilityDropDownList" runat="server" Width="120px"
+         DataSourceID="SqlDataSource1" DataTextField="facility" DataValueField="facility" 
+        ></asp:DropDownList>
+
+         <!-- SQL statement that retrieves all facilities from the database -->
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:ConnectionString %>"
+             SelectCommand="SELECT [facility] FROM [Facility]">
+
+        </asp:SqlDataSource>
 
         <br /><br />
 
+    <asp:Label ID="dateLabel" runat="server" Text="Date"></asp:Label>
+    <asp:TextBox ID="dateTextbox" runat="server" Width="114px"></asp:TextBox>
+    <br /><br />
 
+       <!-- AJAXControl that makes a calendar appear when the dateTextBox is clicked -->
+    <asp:CalendarExtender TargetControlID="dateTextBox" ID="CalendarExtender1" runat="server" 
+        FirstDayOfWeek="Monday" Format="dd/MM-yyyy">
+    </asp:CalendarExtender>
+    
+    <br /><br />
+        
+     
+        <asp:Label ID="TimeLabel" runat="server" Width="60px" Text="Time:"></asp:Label>
+        <asp:DropDownList ID="TimeDropDownList" runat="server" Width="120px" 
+            DataSourceID="SqlDataSource2" DataTextField="time" DataValueField="time" >
+        </asp:DropDownList>
+
+      <!-- SQL statement that retrieves all times from the database -->
+    <asp:SqlDataSource ID="SqlDataSource2" runat="server"
+         ConnectionString="<%$ ConnectionStrings:ConnectionString %>"
+         SelectCommand="SELECT [time] FROM [times]"></asp:SqlDataSource>
+
+       <!-- When this button is clicked the method "BookingButton_Click" in the code behind file is called -->
+   <asp:Button ID="BookingButton" runat="server" Text="Create Booking" onclick="BookingButton_Click" />
+   <br /><br />
+    
+      <asp:Label ID="resultLabel" runat="server" Text=""></asp:Label>
 
 
 </asp:Content>
--- member/BookFacility.aspx.cs
+++ member/BookFacility.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +11,51 @@ public partial class member_BookFacility : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+    }
+
+
+    protected void BookingButton_Click(object sender, EventArgs e)
+    {
+        //Get the date as a string from the dateTextBox
+        string dateStr = dateTextbox.Text;
+
+        //Split the date string on every '-' or '/' found
+        string[] splitDateStr = dateStr.Split(new char[] { '-', '/' });
+
+        //Create a DateTime object from the chosen date. Constructor takes date in this order: year, month, date. So, last index of the array first
+        DateTime theDate = new DateTime(Convert.ToInt32(splitDateStr[2]), Convert.ToInt32(splitDateStr[1]), Convert.ToInt32(splitDateStr[0]));
+
+        // Gets the default connection string/path to our database from the web.config file
+        string dbstring = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        // Creates a connection to our database
+        SqlConnection con = new SqlConnection(dbstring);
+
+        // The SQL statement to insert a booking. By using prepared statements,
+        // we automatically get some protection against SQL injection.
+        string sqlStr = "INSERT INTO bookings (facility, bookedBy, date, time) VALUES (@theFacility, @theUser, @theDate, @theTime)";
+
+        // Open the database connection
+        con.Open();
+
+        // Create an executable SQL command containing our SQL statement and the database connection
+        SqlCommand sqlCmd = new SqlCommand(sqlStr, con);
+
+        // Fill in the parameters in our prepared SQL statement
+        sqlCmd.Parameters.AddWithValue("@theFacility", FacilityDropDownList.SelectedValue);
+        sqlCmd.Parameters.AddWithValue("@theUser", this.User.Identity.Name);
+        sqlCmd.Parameters.AddWithValue("@theDate", theDate);
+        sqlCmd.Parameters.AddWithValue("@theTime", TimeDropDownList.SelectedValue);
+
+        // Execute the SQL command
+        sqlCmd.ExecuteNonQuery();
+
+        // Close the connection to the database
+        con.Close();
+
+        // Show the user that the booking has been added
+        resultLabel.Text = "Booking added";
 
     }
 }
